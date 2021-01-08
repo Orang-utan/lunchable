c7918c4e8db9ab53abdebdc6415ec19707e3fc1e
Daniel Tian <dtian.95@gmail.com>
2021-01-09 02:48:04 +0800

    rebase

--- extension/src/popup/index.js
+++ extension/src/popup/index.js
@@ -1,9 +1,5 @@
 import ReactDOM from "react-dom";
-import AppRouter from "./pages/AppRouter";
 import { initAmplitude } from "./util/amplitude";
-
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
 
 initAmplitude();
 
