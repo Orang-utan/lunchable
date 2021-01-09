8e35a4cd04e610836eae2337a7939a01e29a488a
Kevin Z <zkw917@gmail.com>
2021-01-09 17:01:23 +0800

    Updated formik field

--- web/src/components/FormField.js
+++ web/src/components/FormField.js
@@ -2,16 +2,20 @@ import { Field, ErrorMessage } from "formik";
 import clsx from "clsx";
 
 /** A `<Field />` from Formik with error messages & basic styling. */
-function FormField({ name, type, label, errors }) {
+function FormField({ name, type, label, errors, width }) {
   return (
     <div className="field">
-      <label className="label">{label}</label>
+      <label style={{ marginBottom: "6px" }} className="body">
+        {label}
+      </label>
       <div className="control">
-        <Field
-          name={name}
-          type={type || "text"}
-          className={clsx("input", errors[name] && "is-danger")}
-        />
+        <div style={width ? { width: width } : null}>
+          <Field
+            name={name}
+            type={type || "text"}
+            className={clsx("input", errors[name] && "is-danger")}
+          />
+        </div>
       </div>
       <p className="help is-danger">
         <ErrorMessage name={name} />
--- web/src/pages/setting.js
+++ web/src/pages/setting.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Formik, Form } from "formik";
+import FormField from "../components/FormField";
 
 import "../styles/color.css";
 import "../styles/layout.css";
@@ -6,23 +8,49 @@ import "../styles/typography.css";
 import "../styles/animation.css";
 
 const Setting = () => {
+  async function handleSave({ lunchStart, duration }, actions) {
+    console.log(lunchStart, duration);
+    // validate fields
+  }
+
+  const navigateHome = async () => {
+    await handleSave();
+  };
+
   return (
     <div className="dash-container fade-in">
       <div className="title-container">
         <div className="header3">Setting</div>
       </div>
-      <div>Usual lunch start time</div>
-      <input
-        class="input inputContainer inputDefault"
-        type="text"
-        placeholder="Type in time"
-      />
-      <div>Duration</div>
-      <input
-        class="input inputContainer inputDefault"
-        type="text"
-        placeholder="in minutes"
-      />
+      <Formik initialValues={{ email: "", password: "" }} onSubmit={handleSave}>
+        {({ errors, isSubmitting }) => (
+          <Form>
+            <FormField
+              type="time"
+              name="lunchStart"
+              label="Usual lunch time"
+              errors={errors}
+              className="bodyText"
+              width={300}
+            />
+            <FormField
+              name="duration"
+              label="Duration"
+              errors={errors}
+              type="number"
+              width={300}
+            />
+            <button
+              type="submit"
+              className="fullstretchButton primary-button"
+              style={{ width: "90px" }}
+              disabled={isSubmitting}
+            >
+              Save
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
