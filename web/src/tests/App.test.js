f6c19bb619cfaa86f8698668eb5b8e2fa579fdda
Daniel Tian <dtian.95@gmail.com>
2021-01-10 12:01:20 +0800

    refined test

--- web/src/tests/App.test.js
+++ web/src/tests/App.test.js
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
-import App from "../App";
+import IndexPage from "../pages/IndexPage";
 
 test("renders", () => {
-  render(<App />);
+  render(<IndexPage />);
 });
